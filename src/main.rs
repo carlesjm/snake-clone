7349f918c97d28166c6db8245d0ce6d463f1b5d0
Carles <cjuan@apsl.net>
2021-10-25 08:50:18 +0200

    Initial commit. Empty window

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn main() {
+    App::build()
+    .add_startup_system(setup.system())
+    .add_plugins(DefaultPlugins)
+    .run();
+}
